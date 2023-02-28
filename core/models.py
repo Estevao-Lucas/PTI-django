8523046d2cc5c31c4141ca08bb653a1a323cf940
estevao.luna <estevao.luna@cocobambu.com>
2023-02-28 16:33:27 -0300

    created app models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class Substance(models.Model):
+    name = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    sub_category = models.ForeignKey(
+        SubCategory, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Nature(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Symptoms(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    nature = models.ForeignKey(Nature, on_delete=models.SET_NULL, null=True, blank=True)
+    sub_category = models.ForeignKey(
+        SubCategory, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    weight = models.IntegerField()
+    substance = models.ManyToManyField(Substance)
+
+    def __str__(self):
+        return self.category.name
+
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    mother_name = models.CharField(max_length=100)
+    born_date = models.DateField()
+    symptoms = models.ManyToManyField(Symptoms)
+
+    def __str__(self):
+        return self.name
