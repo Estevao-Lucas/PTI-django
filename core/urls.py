370c82bb859b233b358007cda4a9e99a7422ad54
estevao.luna <estevao.luna@cocobambu.com>
2023-04-27 12:41:14 -0300

    added "/" in the end of endpoints

--- core/urls.py
+++ core/urls.py
@@ -6,15 +6,15 @@ from core.app.api.views.substance_view import SubstanceAPIView, SubstancesAPIVie
 
 urlpatterns = [
     path("symptoms/<int:id>", SymptomAPIView.as_view(), name="symptom"),
-    path("symptoms", SymptomsAPIView.as_view(), name="symptoms"),
-    path("substances", SubstancesAPIView.as_view(), name="substances"),
+    path("symptoms/", SymptomsAPIView.as_view(), name="symptoms"),
+    path("substances/", SubstancesAPIView.as_view(), name="substances"),
     path("substances/<int:id>", SubstanceAPIView.as_view(), name="substance"),
-    path("sub-categories", SubCategoriesAPIView.as_view(), name="sub-categories"),
+    path("sub-categories/", SubCategoriesAPIView.as_view(), name="sub-categories"),
     path(
         "sub-categories/<int:id>",
         SubCategoryAPIView.as_view(),
         name="sub-category",
     ),
-    path("patients", PatientsAPIView.as_view(), name="patients"),
+    path("patients/", PatientsAPIView.as_view(), name="patients"),
     path("patients/<int:id>", PatientAPIView.as_view(), name="patient"),
 ]
