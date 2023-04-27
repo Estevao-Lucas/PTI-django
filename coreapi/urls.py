3b5f8573bcbc1d5ac8cc13a142aa8e958970f833
estevao.luna <estevao.luna@cocobambu.com>
2023-04-27 10:28:42 -0300

    fix bug interface django admin

--- coreapi/urls.py
+++ coreapi/urls.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [path("admin/", admin.site.urls), path("api/", include("core.urls"))]
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
