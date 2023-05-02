b021510f520a4ad862deb8ea954831047ff08fbe
estevao.luna <estevao.luna@cocobambu.com>
2023-05-01 22:32:39 -0300

    added / on the final of endpoints

--- core/urls.py
+++ core/urls.py
@@ -5,16 +5,16 @@ from core.app.api.views.symptom_view import SymptomAPIView, SymptomsAPIView
 from core.app.api.views.substance_view import SubstanceAPIView, SubstancesAPIView
 
 urlpatterns = [
-    path("symptoms/<int:id>", SymptomAPIView.as_view(), name="symptom"),
+    path("symptoms/<int:id>/", SymptomAPIView.as_view(), name="symptom"),
     path("symptoms/", SymptomsAPIView.as_view(), name="symptoms"),
     path("substances/", SubstancesAPIView.as_view(), name="substances"),
-    path("substances/<int:id>", SubstanceAPIView.as_view(), name="substance"),
+    path("substances/<int:id>/", SubstanceAPIView.as_view(), name="substance"),
     path("sub-categories/", SubCategoriesAPIView.as_view(), name="sub-categories"),
     path(
-        "sub-categories/<int:id>",
+        "sub-categories/<int:id>/",
         SubCategoryAPIView.as_view(),
         name="sub-category",
     ),
     path("patients/", PatientsAPIView.as_view(), name="patients"),
-    path("patients/<int:id>", PatientAPIView.as_view(), name="patient"),
+    path("patients/<int:id>/", PatientAPIView.as_view(), name="patient"),
 ]
